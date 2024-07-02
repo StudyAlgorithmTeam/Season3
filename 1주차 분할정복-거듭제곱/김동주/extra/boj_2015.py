519c1c2972c1968d8678a6e129d23c58333358a0
Hepheir <hepheir@gmail.com>
2024-07-03 08:09:43 +0900

    AC(184ms): #prefix_sum #hashing O(N)

--- 1주차 분할정복-거듭제곱/김동주/extra/boj_2015.py
+++ 1주차 분할정복-거듭제곱/김동주/extra/boj_2015.py
@@ -1,5 +1,7 @@
 # 수들의 합 4
 
+from collections import Counter
+
 
 N, K = map(int, input().split())
 A = [*map(int, input().split())]
@@ -10,10 +12,17 @@ for i in range(1, N):
     prefix_sum[i] += prefix_sum[i-1]
 
 cases = 0
+counter = Counter([0])
 for j in range(N):
-    for i in range(j+1):
-        sub_sum = prefix_sum[j] - prefix_sum[i] + A[i]
-        if sub_sum == K:
-            cases += 1
+    k = prefix_sum[j] - K
+    # prefix_sum[i] (0 <= i < j) 이 k 인 것의 개수를 구해야 한다.
+    #
+    # N은 최대 200,000이므로, prefix_sum[i] = k인 것의 개수를 찾는데
+    # O(log N) 이하의 알고리즘을 사용해야 함.
+    #
+    # dict에 기반한 Counter는 set, get에 hash를 사용하므로, 평균적으로 O(1)이 걸린다.
+    # https://wiki.python.org/moin/TimeComplexity
+    cases += counter[k]
+    counter[prefix_sum[j]] += 1
 
 print(cases)
