6eec2b77aef0befcbcdd93523768b90c807bc80c
Kwon <160165670+Kwondongkyun@users.noreply.github.com>
2024-07-09 00:53:19 +0900

    Create 13171_A(S3).py

---
+++ 2주차 Knapsack/권동균/extra/13171_A(S3).py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+a=int(input())
+b=int(input())
+
+c=int(1e+9)+7
+def gob(a,b,c):
+    if b==1: return a%c
+    elif b%2==0:
+        return pow(gob(a%c, b//2, c),2)%c
+    else:
+        return pow(gob(a%c, b//2, c),2)*a%c
+        
+print(gob(a,b,c))
