c2620a8ae15a942645dedc4fd58c924149edb854
Hepheir <hepheir@gmail.com>
2024-07-13 04:20:16 +0900

    AC(624ms): #deque O(N)

--- 3주차 DP/김동주/extra/boj_18115.py
+++ 3주차 DP/김동주/extra/boj_18115.py
@@ -7,23 +7,21 @@ N = int(input())
 A = input().split()
 
 # double-ended queue
-# 0번 인덱스가 바닥이다.
+# 0번 인덱스가 위이다.
 dq = deque()
 
 for i in range(1, N+1):
     match A.pop():
         # 제일 위의 카드 1장을 바닥에 내려놓는다.
         case "1":
-            dq.append(i)
+            dq.appendleft(i)
 
         # 위에서 두 번째 카드를 바닥에 내려놓는다. 카드가 2장 이상일 때만 쓸 수 있다.
         case "2":
-            top = dq.pop()
-            dq.append(i)
-            dq.append(top)
+            dq.insert(1, i)
 
         # 제일 밑에 있는 카드를 바닥에 내려놓는다. 카드가 2장 이상일 때만 쓸 수 있다.
         case "3":
-            dq.appendleft(i)
+            dq.append(i)
 
-print(*reversed(dq))
+print(*dq)
