d6b51a45d63dba6e69e26caded169fbfaf7567cd
Kwon <160165670+Kwondongkyun@users.noreply.github.com>
2024-08-06 16:36:21 +0900

    Update 1654_랜선 자르기(S2).py

--- 6주차/권동균/1654_랜선 자르기(S2).py
+++ 6주차/권동균/1654_랜선 자르기(S2).py
@@ -13,14 +13,14 @@ start=1
 #O(K) (l : K개의 원소)
 end=max(l)
 
-# O(log(n)) -> n : max(l)
+# O(log(max(l)))
 # 이진 탐색 : start가 end보다 작거나 같을 때까지 반복
 # -> 매 루프마다 탐색 범위 절반으로 줄어듬 --> O(log(n))
 while start<=end:
-    cnt=0
     mid=(start+end)//2
     # O(K)
     # 리스트(l)의 각 랜선을 mid값으로 나누어 더함
+    cnt=0
     for i in l:
         cnt+=i//mid
     if cnt<N:
@@ -29,5 +29,12 @@ while start<=end:
         start=mid+1
 print(end)
 
-# 이진 탐색 루프 : O(log N), 랜선 자르기 작업 : O(K)
-# ∴ 전체 시간복잡도 : O(K log(N))
+# 이진 탐색 루프 : O(log max(l)), 랜선 자르기 작업 : O(K)
+# ∴ 전체 시간복잡도 : O(K log(max(l)))
+
+
+
+# max(l) = 2^31-1
+# log(max(l)) = 21.487...
+# K=10000
+# K log(max(l)) = 214875.6259 ...
