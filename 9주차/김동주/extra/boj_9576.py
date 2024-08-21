749617d3c572abde28768f48125bcd0a77ffa7e5
Hepheir <hepheir@gmail.com>
2024-08-22 01:16:17 +0900

    AC(80ms): #greedy #sorting O(T(M log M))

--- 9주차/김동주/extra/boj_9576.py
+++ 9주차/김동주/extra/boj_9576.py
@@ -8,18 +8,23 @@ for _ in range(T):
     # 회의실 배정 문제로 치환하여 풀이.
     # 최대 몇 개의 회의를 진행할 수 있을까!
     N, M = map(int, sys.stdin.readline().split())
+
     timelines = []
     for i in range(M):
         start, end = map(int, sys.stdin.readline().split())
-        timelines.append((start, end))
-    timelines.sort()
-    now = 0
-    count = 0
+        timelines.append([start, end])
+    timelines.sort(key=lambda x: (x[1], x[0]))
+
+    selected = [False] * (N+1)
     for start, end in timelines:
-        if now < start:
-            now = start
+        for n in range(start, end+1):
+            if not selected[n]:
+                selected[n] = True
+                break
+
+    count = 0
+    for n in range(1, N+1):
+        if selected[n]:
             count += 1
-        elif start <= now < end:
-            now = now + 1
-            count += 1
+
     sys.stdout.write(f'{count}\n')
